a17504ddf02e7fe2fbd6740bb23e225d0298aad8
Timothy Warren <twarren@nabancard.com>
2022-12-06 13:54:34 -0500

    Complete day 6

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,37 +1,50 @@
-fn find_num_chars_before_marker(chars: &Vec<char>) -> usize {
-    let mut iter = chars.iter().enumerate().peekable();
+fn find_num_chars_before_marker(chars: &Vec<char>, marker_size: usize) -> usize {
+    let mut iter = chars.iter().enumerate();
 
-    for (i, _) in &mut iter {
-        let zero = chars.get(i);
-        let one = chars.get(i + 1);
-        let two = chars.get(i + 2);
-        let three = chars.get(i + 3);
-
-        let mut cursor: Vec<char> = vec![*zero.unwrap(), *one.unwrap(), *two.unwrap(), *three.unwrap()];
+    'outer: for (i, _) in &mut iter {
+        // Look marker_size -1 characters forward so we can check for duplicate characters
+        let mut cursor: Vec<char> = Vec::new();
+        for n in 0..marker_size {
+            cursor.push(*chars.get(i + n).unwrap());
+        }
         cursor.sort_unstable();
 
         // If there's a duplicate character, go forward
-        if cursor[0] == cursor[1] || cursor[1] == cursor[2] || cursor[2] == cursor[3] || i < 4 {
-            continue;
+        for (n, ch) in cursor.iter().enumerate() {
+            if let Some(other) = cursor.get(n + 1) {
+                if ch == other {
+                    continue 'outer;
+                }
+            }
         }
 
-        // Since we are looking 3 characters farther than the current iteration,
-        // we need to add that to the returned index, plus an extra one, because
-        // the array is zero-indexed
-        return i + 4;
+        // Since we are looking farther than the current iteration,
+        // we need to add the marker size that to the returned index
+        return i + marker_size;
     }
 
     panic!("Marker not found");
+}
+
+fn find_packet_marker(chars: &Vec<char>) -> usize {
+    find_num_chars_before_marker(chars, 4)
+}
+
+fn find_message_marker(chars: &Vec<char>) -> usize {
+    find_num_chars_before_marker(chars, 14)
 }
 
 fn main() {
     let file_str = include_str!("input.txt");
     let chars: Vec<char> = file_str.chars().collect();
 
-    let chars_before_marker = find_num_chars_before_marker(&chars);
-
     println!(
         "Part 1: Number of characters before start-of-packet marker: {}",
-        chars_before_marker
+        find_packet_marker(&chars)
+    );
+
+    println!(
+        "Part 2: Number of characters before start-of-message marker: {}",
+        find_message_marker(&chars)
     );
 }
