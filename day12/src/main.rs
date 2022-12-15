6c8a850864f16c573c583ae5eb8a7f4d832f05d9
Timothy Warren <twarren@nabancard.com>
2022-12-15 16:53:02 -0500

    A touch more of a start to day 12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let file_str = include_str!("input.txt");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_test_data() -> &'static str {
+        include_str!("test-input.txt")
+    }
 }
